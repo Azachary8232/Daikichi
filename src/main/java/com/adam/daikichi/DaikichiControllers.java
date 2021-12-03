c3204fb539bbe6f89fdb9059f504125112bfbec6
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-12-03 16:28:56 -0700

    Daikichi Path Variables Complete

--- src/main/java/com/adam/daikichi/DaikichiControllers.java
+++ src/main/java/com/adam/daikichi/DaikichiControllers.java
@@ -31,8 +31,13 @@ public class DaikichiControllers {
 	}
 	
 	@RequestMapping("/lotto/{lotto}")
-	public String lotto(@PathVariable("lotto") String lotto) {
-		return "You have enjoyed the fruits of your labor but now is a great time to spend with your family and friends"; 
+	public String lotto(@PathVariable("lotto") Integer lotto) {
+		if( lotto %2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend with your family and friends."; 
+		}
 	}
 
 	
